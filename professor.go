98b0542cf0278b3f3f1511e6d1d0da41d941a5f5
arthurh0812 <arthurh1204@gmail.com>
2021-04-10 18:58:15 +0200

    professor struct added

--- professor.go
+++ professor.go
@@ -1,5 +1,17 @@
 package university
 
 type Professor struct {
+	*Person
 	Salary float32
+}
+
+func NewProfessor(p *Person, salary float32) *Professor {
+	return &Professor{
+		Person: p,
+		Salary: salary,
+	}
+}
+
+func (p *Professor) SetFirstName() string {
+	return ""
 }
